cb021b75269d90cdcaec772a945a06b8e9572c6d
chiefmouse <chiefmouse@newsmice.co.uk>
2013-09-03 18:53:08 +0100

    Update project.clj
    Reflects fork rename, updates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject faker "0.2.2"
-  :description "Generate fake data, port of ruby faker"
-  :url "http://github.com/paraseba/faker"
+(defproject faker-UK "0.2.3"
+  :description "Generate fake data, fork of faker, itself a port of ruby faker"
+  :url "http://github.com/chiefmouse/faker-UK"
   :dependencies [[org.clojure/clojure "1.2.0"]])
