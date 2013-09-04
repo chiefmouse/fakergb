a524121741fb85558c75a30915f17422c0dcda89
Bob Marshall <chiefmouse@newsmice.co.uk>
2013-09-04 12:09:27 +0100

    Added clojars pgp signing

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject fakerUK "0.2.3"
   :description "Generate fake data, fork of faker, itself a port of ruby faker"
   :url "http://github.com/chiefmouse/fakerUK"
-  :dependencies [[org.clojure/clojure "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]]
+  {:signing {:gpg-key "chiefmouse@newsmice.co.uk"}} 
+)
